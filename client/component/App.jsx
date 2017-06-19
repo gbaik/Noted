04ccf687cc3e887e69789500ab777d73da4d0e70
gbaik <gideonbaik@gmail.com>
2017-06-19 12:30:46 -0700

    Add basic template for saving/writing notes

---
+++ client/component/App.jsx
@@ -0,0 +1,19 @@
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Noted</h1>
+        <button>Add</button>
+        <div>
+          <h3>Title</h3>
+          <input></input>
+        </div>
+        <div>
+          <h3>Notes</h3>
+          <input></input>
+        </div>
+      </div>
+    );
+  }
+}
