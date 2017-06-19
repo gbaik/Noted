6873921f8bd92b11a28b23bc26e2c25d76df260d
gbaik <gideonbaik@gmail.com>
2017-06-19 16:03:37 -0700

    Add function to receive title and notes from user

--- client/component/NewNote.jsx
+++ client/component/NewNote.jsx
@@ -1,17 +1,47 @@
 class NewNote extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'test',
+      note: 'test'
+    };
+
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleNoteChange = this.handleNoteChange.bind(this);
+    this.handleSavingNotes = this.handleSavingNotes.bind(this);
+  }
+
+  handleSavingNotes() {
+    console.log('title:',this.state.title);
+    console.log('note:',this.state.note);
+  }
+
+  handleTitleChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  handleNoteChange(event) {
+    this.setState({
+      note: event.target.value
+    });
+  }
+
 
   render() {
     return (
       <div>
         <h1>New Notes</h1>
-        <button>Save</button>
+        <button onClick={this.handleSavingNotes}>Save</button>
         <div>
           <h3>Title</h3>
-          <input></input>
+          <input onChange={this.handleTitleChange}></input>
         </div>
         <div>
           <h3>Notes</h3>
-          <input></input> 
+          <textarea onChange={this.handleNoteChange} className="note"></textarea>
         </div>
       </div>
     );
