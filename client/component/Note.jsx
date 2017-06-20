e134811167de285e9aae1bdf05b11c5326f08249
gbaik <gideonbaik@gmail.com>
2017-06-19 23:54:08 -0700

    Add basic note edit function

--- client/component/Note.jsx
+++ client/component/Note.jsx
@@ -1,6 +1,35 @@
-var Note = ({noteTitle, noteEntry}) => (
-  <div>
-    <h3>{noteTitle} </h3>
-    {noteEntry}
-  </div>
-)
+var Note = ({noteTitle, noteEntry}) => {
+  var editNote = (title, entry) => {
+    findNote(title, entry, (note) => {
+      console.log(note);
+    });
+  };
+
+  var findNote = (title, entry, callback) => {
+    console.log(title, entry);
+    $.ajax({
+      url: '/client/editNote',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('GET request success');
+        callback(data);
+      },
+      error: function() {
+        console.log('GET request failure');
+      }
+    })
+  };
+
+  return(
+    <div>
+      <button onClick={() => editNote(noteTitle, noteEntry)}>{noteTitle}</button>
+      {noteEntry}
+    </div>
+  )
+}
+
+/*
+  Notes must have title in the url link
+  Acess the notes and edit them
+*/
