dd1c98bad7f620c9f8c4bdf5bb013b59622b7c5c
Gideon Baik <gideonbaik@gmail.com>
2018-01-03 17:35:09 -0800

    Add test setup

--- server/index.py
+++ server/index.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+app = Flask(__name__, static_folder = '../static/dist', template_folder = '../static/')
 
 @app.route('/')
 def index():
   return render_template("index.html")
 
+print('hello world')
+
 if __name__ == "__main__":
-  app.run(debug = True)
+  app.run(debug = True) 
---
+++ test/test_server.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import pytest
+
+sys.path.append(os.getcwd() + '/server')
+
+import index
+
+def test_connection():
+  pass 
+
+if __name__ == '__main__':
+  test_connection()
