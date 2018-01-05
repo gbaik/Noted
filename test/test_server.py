360082b9e4356b21249749335101652d98b01df7
Gideon Baik <gideonbaik@gmail.com>
2018-01-04 17:59:01 -0800

    Add basic test function framework

--- test/test_server.py
+++ test/test_server.py
@@ -7,7 +7,19 @@ sys.path.append(os.getcwd() + '/server')
 import index
 
 def test_connection():
-  pass 
+  pass
+
+def test_getAllNotes():
+  pass
+
+def test_getOneNote():
+  pass
+
+def test_addNote():
+  pass
+
+def test_editNote():
+  pass
 
 if __name__ == '__main__':
   test_connection()
